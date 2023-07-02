fe1889aba6823a76c3c1bd8eb482e490e327ca0c
DEEPAK67578 <calisttoha@gmail.com>
2023-07-02 10:21:54 +0530

    added Basic Components

---
+++ src/components/Expenses.js
@@ -0,0 +1,31 @@
+import Expenseitem from "./Expenseitem";
+import './Expenses.css'
+function Expenses(props) {
+    console.log(props.data)
+    return (
+        <div className="expenses">
+         <Expenseitem
+        title={props.data[0].title}
+        amount={props.data[0].amount}
+        date={props.data[0].date}
+      ></Expenseitem>
+      <Expenseitem
+        title={props.data[1].title}
+        amount={props.data[1].amount}
+        date={props.data[1].date}
+      ></Expenseitem>
+      <Expenseitem
+        title={props.data[2].title}
+        amount={props.data[2].amount}
+        date={props.data[2].date}
+      ></Expenseitem>
+      <Expenseitem
+        title={props.data[3].title}
+        amount={props.data[3].amount}
+        date={props.data[3].date}
+      ></Expenseitem>
+        </div>
+    )
+}
+
+export default Expenses
